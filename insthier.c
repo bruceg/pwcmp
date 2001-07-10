111b79f5f494ecff2d924d6ce2f614d50f4e73ed
Bruce Guenter <bruce@untroubled.org>
2001-07-10 10:33:32 +0000

    Added new MD5 password comparison module.

--- insthier.c
+++ insthier.c
@@ -4,6 +4,7 @@
 void insthier(void) {
   int bin = opendir(conf_bin);
   c(bin, 0, "pwcmp-crypt", -1, -1, 0755);
+  c(bin, 0, "pwcmp-md5",   -1, -1, 0755);
   c(bin, 0, "pwcmp-plain", -1, -1, 0755);
   c(bin, 0, "pwcmp-test",  -1, -1, 0755);
 }
