a1c1f48f8b3128a846fd00ed3f65f6ddf3f64e84
Bruce Guenter <bruce@untroubled.org>
2001-07-10 05:24:23 +0000

    Added a bunch of distribution files.

---
+++ insthier.c
@@ -0,0 +1,9 @@
+#include "installer.h"
+#include "conf_bin.c"
+
+void insthier(void) {
+  int bin = opendir(conf_bin);
+  c(bin, 0, "pwcmp-crypt", -1, -1, 0755);
+  c(bin, 0, "pwcmp-plain", -1, -1, 0755);
+  c(bin, 0, "pwcmp-test",  -1, -1, 0755);
+}
